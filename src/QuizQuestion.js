c1736b2a9c13223382816e31a5d07f15f0b5fb5f
Przemysław Jurewicz <przemyslaw.jurewicz@newday.co.uk>
2021-09-24 10:36:30 +0200

    feat: mod 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component{
+  constructor(props){
+    super(props)
+    this.state = {incorrectAnswer: false}
+  }
 handleClick(buttonText){
-  if (buttonText === this.props.quiz_question.answer)
-  { this.props.showNextQuestionHandler() }
+  if (buttonText === this.props.quiz_question.answer){ 
+    this.props.showNextQuestionHandler()
+    this.setState({incorrectAnswer: false })
+  }
+  else
+  this.setState({incorrectAnswer: true })
 }
+
 render(){
     return(
         <main>
@@ -18,6 +27,8 @@ render(){
             <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)}/>)}
           </ul>
         </section>
+        { this.state.incorrectAnswer && <p className='error'>Sorry, that's not right</p> }
+      
       </main>
     )
 }
