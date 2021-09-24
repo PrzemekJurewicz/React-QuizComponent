62591971e65bf691051e7dcf30c08a254c31c0ab
Przemysław Jurewicz <przemyslaw.jurewicz@newday.co.uk>
2021-09-24 10:03:00 +0200

    feat: mod 5

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component{
+    handleClick(button_text){
+        this.props.clickHandler(button_text)
+    }
     render()
     {
         return(
